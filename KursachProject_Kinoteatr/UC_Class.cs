24965857b784159d3769af2353971e2d00b4a123
Nikita Egorov <fast.hof@yandex.ru>
2021-12-02 12:41:22 +0300

    class

---
+++ KursachProject_Kinoteatr/UC_Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursachProject_Kinoteatr
+{
+    class UC_Class
+    {
+        int a;
+    }
+}
