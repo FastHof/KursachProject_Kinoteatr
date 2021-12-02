37ca1bfe8fea1ca926b7cea0fae918aa0674eaba
Nikita Egorov <fast.hof@yandex.ru>
2021-12-02 12:48:11 +0300

    class

---
+++ KursachProject_Kinoteatr/Library.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursachProject_Kinoteatr
+{
+    class Library
+    {
+        int a;
+    }
+}
